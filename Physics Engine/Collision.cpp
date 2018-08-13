1e44e455b26840731acf6bb9ef25ec744495a99b
mattmac12 <m.maclennan@hotmail.com>
2018-08-13 13:02:31 +0800

    Updated OBB collision
    Removed additional functions and implemented them into a single function to make it easier to extract contact point

--- Physics Engine/Collision.cpp
+++ Physics Engine/Collision.cpp
@@ -30,62 +30,116 @@ bool Collision::sphereSphere(const sphere &s1, const sphere &s2, collisionData *
 	return true;
 }
 
-void Collision::SATtest(const v3 &axis, std::vector<v3> vertices, double min, double max)
+bool Collision::OBBOBB(const OBB &s1, const OBB &s2, collisionData *data)
 {
-	for (int i = 0; i < vertices.size(); i++)
+	double dotprod;
+	double s1minn1, s1maxn1, s2minn1, s2maxn1; // Shape 1 min value of normal 1
+	v3 s1minn1c, s1maxn1c, s2minn1c, s2maxn1c; // Shape 1 min value of normal 1 coord
+
+	for (int i = 0; i < s1.normals.size(); i++)
 	{
-		double dotprod = vertices[i].dot(axis);
+		s1minn1 = s1.vertices[0].dot(s1.normals[i]);
+		s1maxn1 = s1.vertices[0].dot(s1.normals[i]);
 
-		if (dotprod < min)
-			min = dotprod;
-		if (dotprod > max)
-			max = dotprod;
-		// need to return min + max
-	}
-}
+		for (int j = 1; j < s1.vertices.size(); j++)
+		{
+			dotprod = s1.vertices[j].dot(s1.normals[i]);
 
-bool Collision::OBBOBB(const OBB &s1, const OBB &s2)
-{
-	// Default first value.
-	double s1minn1 = s1.vertices[0].dot(s1.normals[0]); // Shape 1 min for normal of s1
-	double s1maxn1 = s1.vertices[0].dot(s1.normals[0]);
-	double s2minn1 = s2.vertices[0].dot(s1.normals[0]);
-	double s2maxn1 = s2.vertices[0].dot(s1.normals[0]);
+			if (dotprod < s1minn1)
+			{
+				s1minn1 = dotprod;
+				s1minn1c = s1.vertices[j];
+			}
 
-	// Loop through remaining normals and find mins and maxes.
-	for (int i = 1; i < s1.normals.size(); i++)
-	{
-		SATtest(s1.normals[i], s1.vertices, s1minn1, s1maxn1);
-		SATtest(s1.normals[i], s2.vertices, s2minn1, s2maxn1);
+			if (dotprod > s1maxn1)
+			{
+				s1maxn1 = dotprod;
+				s1maxn1c = s1.vertices[j];
+			}
+		}
 
-		if (!overlaps(s1minn1, s1maxn1, s2minn1, s2maxn1))
+		s2minn1 = s2.vertices[0].dot(s1.normals[i]);
+		s2maxn1 = s2.vertices[0].dot(s1.normals[i]);
+
+		for (int j = 1; j < s2.vertices.size(); j++)
+		{
+			dotprod = s2.vertices[j].dot(s1.normals[i]);
+
+			if (dotprod < s2minn1)
+			{
+				s2minn1 = dotprod;
+				s2minn1c = s2.vertices[j];
+			}
+
+			if (dotprod > s2maxn1)
+			{
+				s2maxn1 = dotprod;
+				s2maxn1c = s2.vertices[j];
+			}
+		}
+
+		if (s1minn1 <= s2minn1 && s2minn1 <= s1maxn1 || s1minn1 <= s1minn1 && s1minn1 <= s2maxn1)
+		{
+			data->contactPoint = (s2maxn1c - s2minn1) + (s1maxn1c - s1minn1c);
+		}
+		else
 			return false;
 	}
 
-	// Default first value.
-	double s1minn2 = s1.vertices[0].dot(s2.normals[0]); // Shape 1 min for normal of s2
-	double s1maxn2 = s1.vertices[0].dot(s2.normals[0]);
-	double s2minn2 = s2.vertices[0].dot(s2.normals[0]);
-	double s2maxn2 = s2.vertices[0].dot(s2.normals[0]);
-	
-	for (int i = 1; i < s2.normals.size(); i++)
-	{
-		SATtest(s2.normals[i], s1.vertices, s1minn2, s1maxn2);
-		SATtest(s2.normals[i], s2.vertices, s2minn2, s2maxn2);
+	double s1minn2, s1maxn2, s2minn2, s2maxn2;
+	v3 s1minn2c, s1maxn2c, s2minn2c, s2maxn2c;
 
-		if (!overlaps(s1minn2, s1maxn2, s2minn2, s2maxn2))
+	for (int i = 0; i < s2.normals.size(); i++)
+	{
+		s1minn2 = s1.vertices[0].dot(s2.normals[i]);
+		s1minn2c = s1.vertices[0];
+		s1maxn2 = s1.vertices[0].dot(s2.normals[i]);
+		s1maxn2c = s1.vertices[0];
+
+		for (int j = 1; j < s1.vertices.size(); j++)
+		{
+			dotprod = s1.vertices[j].dot(s2.normals[i]);
+
+			if (dotprod < s1minn2)
+			{
+				s1minn2 = dotprod;
+				s1minn2c = s1.vertices[j];
+			}
+				
+			if (dotprod > s1maxn2)
+			{
+				s1maxn2 = dotprod;
+				s1maxn2c = s1.vertices[j];
+			}
+		}
+
+		s2minn2 = s2.vertices[0].dot(s2.normals[i]);
+		s2maxn2 = s2.vertices[0].dot(s2.normals[i]);
+
+		for (int j = 1; j < s2.vertices.size(); j++)
+		{
+			dotprod = s2.vertices[j].dot(s2.normals[i]);
+
+			if (dotprod < s2minn2)
+			{
+				s2minn2 = dotprod;
+				s2minn2c = s2.vertices[j];
+			}
+
+			if (dotprod > s2maxn2)
+			{
+				s2maxn2 = dotprod;
+				s2maxn2c = s2.vertices[j];
+			}
+		}
+
+		if (s1minn2 <= s2minn2 && s2minn2 <= s1maxn2 || s1minn2 <= s1minn2 && s1minn2 <= s2maxn2)
+		{
+			data->contactPoint = (s2maxn2c - s2minn2) + (s1maxn2c - s1minn2c);
+		}
+		else
 			return false;
 	}
 
 	return true;
-}
-
-bool Collision::overlaps(double min1, double max1, double min2, double max2)
-{
-	return isBetweenOrdered(min2, min1, max1) || isBetweenOrdered(min1, min2, max2);
-}
-
-bool Collision::isBetweenOrdered(double val, float lower, float upper)
-{
-	return lower <= val && val <= upper;
 }
--- Physics Engine/Collision.h
+++ Physics Engine/Collision.h
@@ -22,19 +22,16 @@ public:
 
 struct collisionData
 {
-	sphere *boundingSpheres[2];
 	v3 contactPoint;
 	bool collided = false;
 };
 
+
 class Collision
 {
 public:
-	bool sphereSphere(const sphere &s1, const sphere &s2, collisionData *data);
+	static bool sphereSphere(const sphere &s1, const sphere &s2, collisionData *data);
 
-	void SATtest(const v3 &axis, std::vector<v3> vertices, double min, double max);
-	bool OBBOBB(const OBB &s1, const OBB &s2);
-	bool overlaps(double min1, double max1, double min2, double max2);
-	bool isBetweenOrdered(double val, float lower, float upper);
+	bool OBBOBB(const OBB &s1, const OBB &s2, collisionData *data);
 };
 
